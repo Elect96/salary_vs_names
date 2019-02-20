a38e844ac92ee3837455d0b1cb86aa46926ea218
Jakub Lamczyk <lamczykjakub@gmail.com>
2019-02-20 17:45:48 +0000

    Introduction of [a,-b] operator regarding lists. Thanks to that I deleted two extreme values from both sides of the list.
    Ref: https://www.pythonforengineers.com/an-introduction-to-numpy-and-matplotlib/

--- __init__.py
+++ __init__.py
@@ -7,15 +7,29 @@ names = np.genfromtxt("names.txt", dtype='str', delimiter=",")
 salary = np.fromfile("salaries.txt", dtype=int, sep=",")
 
 # x contains a list of numbers from 0 to number of names
-x = np.arange(len(names))
-
-plt.bar(x, salary)
-plt.xticks(x, names)
-
-plt.ylabel("Salaries")
-plt.xlabel("Names")
-plt.title("Salary of 10 random people")
-plt.show()
+# x = np.arange(len(names))
+#
+# plt.bar(x, salary)
+# plt.xticks(x, names)
+#
+# plt.ylabel("Salaries")
+# plt.xlabel("Names")
+# plt.title("Salary graph")
+# plt.show()
 
 # numpy supports many functions like below
 # print(np.max(salary), np.min(salary), np.average(salary), np.median(salary))
+
+# [a:-b] - start from a, subtract b from the end of the list
+salaries_new = salary[2:-2]
+names_new = names[2:-2]
+
+x = np.arange(len(names_new))
+
+plt.bar(x, salaries_new)
+plt.xticks(x, names_new)
+
+plt.ylabel("Salaries")
+plt.xlabel("Names")
+plt.title("Salary graph")
+plt.show()
